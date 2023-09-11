cb8a269223551f2203d8e4b438182ae5db4fdb4f
Khai Troy <140205132+khaido117@users.noreply.github.com>
2023-09-11 14:36:13 -0500

    Update main.py

--- main.py
+++ main.py
@@ -28,8 +28,8 @@ def start_timer():
     global reps 
     reps += 1
     work_sec =  WORK_MIN * 60 
-    short_break_sec =   SHORT_BREAK_MIN * 60 
-    long_break_sec =     LONG_BREAK_MIN  * 60 
+    short_break_sec =  SHORT_BREAK_MIN * 60 
+    long_break_sec =   LONG_BREAK_MIN  * 60 
 
 
     count_down(work_sec)
